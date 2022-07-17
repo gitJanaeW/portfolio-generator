062dc24b07f36cfee9432827e21a1a67f19470d9
janae.wel <janae.wel@gmail.com>
2022-07-17 18:21:23 -0400

    troubleshooting

--- app.js
+++ app.js
@@ -14,11 +14,84 @@ const promptUser = () => {
             type: 'input',
             name: 'name',
             message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHUb Username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself'
         }
-    ]);
+    ]).then(projectData => {
+        // determines what will happen if user confirms that they want to add more projects
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
 }
 
-promptUser().then(answers => console.log(answers));
+const promptProject = portfolioData => {
+    portfolioData.projects = portfolioData.projects || [];
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+        },
+        { // No/all choces are both valid entries with 'checkbox'
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node.js']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ]);
+};
+
+// Using .then here instead of in the functions themselves allows us to better control when the console.log is deployed
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
+
+
+
+
+
+
 // const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(userName, gitHub);
